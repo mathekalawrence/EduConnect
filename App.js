aefb722df4642426858cd425bbfd96c366761373
Matheka Lawrence <90851966+mathekalawrence@users.noreply.github.com>
2025-10-19 21:11:18 +0300

    Update App.js

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { StatusBar } from 'expo-status-bar';
-import { AuthProvider, useAuth } from './src/context/AuthContext';
+import { AuthProvider, useAuth } from './src/context/AuthContext'; 
 import { ChatProvider } from './src/context/ChatContext';
 import { EduProvider } from './src/context/EduContext'; 
 
